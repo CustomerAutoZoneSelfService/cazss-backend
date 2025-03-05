a502518e9ad5ba44c12bfec6164f3f2e456f9858
LouArcTec <a01562967@tec.mx>
2025-03-04 19:54:38 -0600

    feature: initial commit (TD-000)

---
+++ src/main/java/com/autozone/cazss_backend/CazssBackendApplication.java
@@ -0,0 +1,13 @@
+package com.autozone.cazss_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CazssBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CazssBackendApplication.class, args);
+	}
+
+}
