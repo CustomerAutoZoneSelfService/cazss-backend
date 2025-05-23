821960e66fd25e64ed0503212ae8f15552f8f11d
Diego Ramirez <diegomadriz004@hotmail.com>
2025-05-23 11:39:38 -0600

    feat: Added Get and Post services for UserFilters (SCRUM-120)

--- src/main/java/com/autozone/cazss_backend/DTO/RequestUserFilterDTO.java
+++ src/main/java/com/autozone/cazss_backend/DTO/RequestUserFilterDTO.java
@@ -1,21 +1,32 @@
 package com.autozone.cazss_backend.DTO;
 
 import jakarta.validation.constraints.NotNull;
+import java.util.List;
 
 public class RequestUserFilterDTO {
-  @NotNull private Integer responsePatternId;
+  @NotNull private List<Integer> responsePatternIds;
+  @NotNull private Integer endpointId;
 
   public RequestUserFilterDTO() {}
 
-  public RequestUserFilterDTO(Integer responsePatternId) {
-    this.responsePatternId = responsePatternId;
+  public RequestUserFilterDTO(List<Integer> responsePatternIds, Integer endpointId) {
+    this.responsePatternIds = responsePatternIds;
+    this.endpointId = endpointId;
   }
 
-  public Integer getResponsePatternId() {
-    return responsePatternId;
+  public List<Integer> getResponsePatternIds() {
+    return responsePatternIds;
   }
 
-  public void setResponsePatternId(Integer responsePatternId) {
-    this.responsePatternId = responsePatternId;
+  public void setResponsePatternIds(List<Integer> responsePatternIds) {
+    this.responsePatternIds = responsePatternIds;
+  }
+
+  public Integer getEndpointId() {
+    return endpointId;
+  }
+
+  public void setEndpointId(Integer endpointId) {
+    this.endpointId = endpointId;
   }
 }
--- src/main/java/com/autozone/cazss_backend/repository/UserFilterRepository.java
+++ src/main/java/com/autozone/cazss_backend/repository/UserFilterRepository.java
@@ -1,7 +1,20 @@
 package com.autozone.cazss_backend.repository;
 
+import com.autozone.cazss_backend.entity.ResponsePatternEntity;
 import com.autozone.cazss_backend.entity.UserFilterEntity;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserFilterRepository
-    extends JpaRepository<UserFilterEntity, UserFilterEntity.UserFilterId> {}
+        extends JpaRepository<UserFilterEntity, UserFilterEntity.UserFilterId> {
+
+    //Help from Pedro Ruiz de la Peña in structuring the right queries
+    List<UserFilterEntity> findByUser_UserIdAndResponsePattern_Response_Endpoint_EndpointId(
+            Integer userId, Integer endpointId);
+
+    void deleteByUser_UserIdAndResponsePattern_Response_Endpoint_EndpointId(
+            Integer userId, Integer endpointId);
+
+    List<ResponsePatternEntity> findByResponsePattern_Response_Endpoint_EndpointId(
+            Integer endpointId);
+}
