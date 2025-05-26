6b308b3bbf47d5bdfaf206803a7749796ea08cc0
Pedro Ruiz de la Pena <pedro.rpg@icloud.com>
2025-05-25 20:25:33 -0600

    chore: Added JavaDoc (SCRUM-126)

--- src/main/java/com/autozone/cazss_backend/DTO/RequestUserFilterDTO.java
+++ src/main/java/com/autozone/cazss_backend/DTO/RequestUserFilterDTO.java
@@ -3,29 +3,60 @@ package com.autozone.cazss_backend.DTO;
 import jakarta.validation.constraints.NotNull;
 import java.util.List;
 
+/**
+ * Data Transfer Object for requesting the creation of user filters. It contains a list of response
+ * pattern IDs and the endpoint ID to which they apply.
+ */
 public class RequestUserFilterDTO {
   @NotNull private List<Integer> responsePatternIds;
   @NotNull private Integer endpointId;
 
+  /** Default constructor. */
   public RequestUserFilterDTO() {}
 
+  /**
+   * Constructs a new RequestUserFilterDTO with the specified response pattern IDs and endpoint ID.
+   *
+   * @param responsePatternIds A list of IDs for the response patterns.
+   * @param endpointId The ID of the endpoint.
+   */
   public RequestUserFilterDTO(List<Integer> responsePatternIds, Integer endpointId) {
     this.responsePatternIds = responsePatternIds;
     this.endpointId = endpointId;
   }
 
+  /**
+   * Gets the list of response pattern IDs.
+   *
+   * @return A list of response pattern IDs.
+   */
   public List<Integer> getResponsePatternIds() {
     return responsePatternIds;
   }
 
+  /**
+   * Sets the list of response pattern IDs.
+   *
+   * @param responsePatternIds A new list of response pattern IDs.
+   */
   public void setResponsePatternIds(List<Integer> responsePatternIds) {
     this.responsePatternIds = responsePatternIds;
   }
 
+  /**
+   * Gets the endpoint ID.
+   *
+   * @return The endpoint ID.
+   */
   public Integer getEndpointId() {
     return endpointId;
   }
 
+  /**
+   * Sets the endpoint ID.
+   *
+   * @param endpointId The new endpoint ID.
+   */
   public void setEndpointId(Integer endpointId) {
     this.endpointId = endpointId;
   }
--- src/main/java/com/autozone/cazss_backend/DTO/UserFilterDTO.java
+++ src/main/java/com/autozone/cazss_backend/DTO/UserFilterDTO.java
@@ -1,18 +1,38 @@
 package com.autozone.cazss_backend.DTO;
 
+/**
+ * Data Transfer Object for user filter information. Represents a single filter criterion based on a
+ * response pattern.
+ */
 public class UserFilterDTO {
   private Integer responsePatternId;
 
+  /**
+   * Constructs a new UserFilterDTO with the specified response pattern ID.
+   *
+   * @param responsePatternId The ID of the response pattern.
+   */
   public UserFilterDTO(Integer responsePatternId) {
     this.responsePatternId = responsePatternId;
   }
 
+  /** Default constructor. */
   public UserFilterDTO() {}
 
+  /**
+   * Gets the ID of the response pattern.
+   *
+   * @return The response pattern ID.
+   */
   public Integer getResponsePatternId() {
     return responsePatternId;
   }
 
+  /**
+   * Sets the ID of the response pattern.
+   *
+   * @param responsePatternId The new response pattern ID.
+   */
   public void setResponsePatternId(Integer responsePatternId) {
     this.responsePatternId = responsePatternId;
   }
