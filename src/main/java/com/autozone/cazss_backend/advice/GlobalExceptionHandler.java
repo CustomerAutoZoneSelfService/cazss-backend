70770fd8109a3078205b50d41ae98422dac0f862
Gustavo B <117482958+DevEquinox@users.noreply.github.com>
2025-06-04 12:59:22 -0600

    feat: add UnauthorizedUserException (UCC-8)
    chore: fix wrong return type for findByCategory_CategoryId (UCC-8)

--- src/main/java/com/autozone/cazss_backend/advice/GlobalExceptionHandler.java
+++ src/main/java/com/autozone/cazss_backend/advice/GlobalExceptionHandler.java
@@ -42,6 +42,21 @@ public class GlobalExceptionHandler {
     return new ResponseEntity<>(error, HttpStatus.UNPROCESSABLE_ENTITY);
   }
 
+  @ExceptionHandler(
+      UnauthorizedUserException.class) // Excepción de acceso no autorizado a un endpoint
+  public ResponseEntity<Object> handleValidationException(final UnauthorizedUserException ex) {
+    // Crear el objeto ErrorResponse
+    ErrorResponseTemplate error =
+        new ErrorResponseTemplate(
+            "VALIDATION_ERROR",
+            ex.getMessage(),
+            "This feature is only available to administrators.",
+            LocalDateTime.now(),
+            UUID.randomUUID().toString());
+
+    return new ResponseEntity<>(error, HttpStatus.UNAUTHORIZED);
+  }
+
   @ExceptionHandler(AZClientException.class) // Excepción de AZClient
   public ResponseEntity<Object> handleAZClientException(final AZClientException ex) {
     // Crear el objeto ErrorResponse
